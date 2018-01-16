18c1115c7bb0eea9cd816fc5d22fd6edea175470
Back Yu <yhfszb@gmail.com>
2018-01-16 09:31:51 +0800

    log: 继续简化……

--- internal/log/logger.go
+++ internal/log/logger.go
@@ -9,10 +9,10 @@ type LogFunc func(level LevelID, format string, v ...interface{})
 
 var Logf LogFunc = func(level LevelID, format string, v ...interface{}) {
 	msg := fmt.Sprintf(format, v...)
-	fmt.Printf("[%s][%s] %s\n", time.Now().Format("2006-01-02 15:04:05"), level, msg)
+	fmt.Printf("[%s][%s] %s\n", time.Now().Format("2006-01-02 15:04:05"), strMap[level], msg)
 }
 
-type LevelID int
+type LevelID = int
 
 var Level = struct {
 	Emergency LevelID
@@ -43,12 +43,4 @@ var strMap = map[LevelID]string{
 	Level.Notice:    "N",
 	Level.Info:      "I",
 	Level.Debug:     "D",
-}
-
-func (id LevelID) String() string {
-	return strMap[id]
-}
-
-func (id LevelID) Int() int {
-	return int(id)
 }
